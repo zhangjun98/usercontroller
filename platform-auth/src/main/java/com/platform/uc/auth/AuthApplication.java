34c62949f9af28f935798b43cb0dab6f3fb62a5f
hao.yan <yan5845hao@qq.com>
2020-07-30 11:30:02 +0800

    first commit

---
+++ platform-auth/src/main/java/com/platform/uc/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.platform.uc.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 授权登录
+ * @author hao.yan
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
---
+++ platform-uc-service/src/main/java/com/platform/uc/service/UcServiceApplication.java
@@ -0,0 +1,17 @@
+package com.platform.uc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户中心底层
+ * @author hao.yan
+ */
+@SpringBootApplication
+public class UcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcServiceApplication.class, args);
+    }
+
+}
