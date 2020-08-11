c8bc3ac2516a6796db5e5f57551e238f7bf1a910
wf <1017776576@qq.com>
2020-08-11 16:26:38 +0800

    角色基本接口

---
+++ platform-uc-service/src/main/java/com/platform/uc/service/mapper/UcRoleMapper.java
@@ -0,0 +1,13 @@
+package com.platform.uc.service.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.platform.uc.service.vo.UcRole;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UcRoleMapper extends BaseMapper<UcRole>
+{
+
+
+}
---
+++ platform-uc-service/src/main/java/com/platform/uc/service/service/RoleService.java
@@ -0,0 +1,41 @@
+package com.platform.uc.service.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.platform.uc.service.mapper.UcRoleMapper;
+import com.platform.uc.service.vo.UcRole;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service public class RoleService
+{
+	@Autowired UcRoleMapper ucRoleMapper;
+
+	public void insert(UcRole ucRole)
+	{
+		ucRoleMapper.insert(ucRole);
+	}
+
+	public void update(UcRole ucRole)
+	{
+		ucRoleMapper.updateById(ucRole);
+	}
+
+	public UcRole selectBean(Long id)
+	{
+		return ucRoleMapper.selectById(id);
+	}
+
+	public List<UcRole> selectList(String name)
+	{
+		QueryWrapper<UcRole> queryWrapper = new QueryWrapper<>();
+		queryWrapper.eq("state",0);
+		if (StringUtils.isNotEmpty(name))
+		{
+			queryWrapper.like("name", name);
+		}
+		return ucRoleMapper.selectList(queryWrapper);
+	}
+}
