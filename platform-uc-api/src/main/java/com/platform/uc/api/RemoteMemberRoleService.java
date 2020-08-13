60338d5bd4532321df599c1210661dec3703f2de
billfanggs <121272528@qq.com>
2020-08-13 17:20:44 +0800

    update

--- platform-uc-api/src/main/java/com/platform/uc/api/RemoteMemberRoleService.java
+++ platform-uc-api/src/main/java/com/platform/uc/api/RemoteMemberRoleService.java
@@ -1,7 +1,10 @@
 package com.platform.uc.api;
 
 import com.platform.uc.api.vo.request.MemberRoleRequest;
+import com.platform.uc.api.vo.request.QueryRoleUserRequest;
 import com.platform.uc.api.vo.response.MemberRoleResponse;
+import com.platform.uc.api.vo.response.UserResponse;
+import com.ztkj.framework.response.core.BizPageResponse;
 import com.ztkj.framework.response.core.BizResponse;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -13,8 +16,18 @@ import java.util.List;
  * 远程用户角色信息接口
  * @author hao.yan
  */
+<<<<<<< HEAD
 @FeignClient(value = "platform-uc-service", path = "/uc/member/role")
+=======
+@FeignClient(value = "platform-uc-service", path = "/member/role")
+>>>>>>> 207db227880d937addbae9d389a81fc2ede94d37
 public interface RemoteMemberRoleService {
+
+    /**
+     * 查询角色下的用户
+     */
+    @PostMapping("/query")
+    BizPageResponse<UserResponse> selectByConditions(@RequestBody QueryRoleUserRequest request);
 
     @PostMapping
     @ApiOperation(value = "保存实体信息")
