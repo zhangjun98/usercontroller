9a1bcab69b1ee556147e8018f529d3a5385dd427
hao.yan <yan5845hao@qq.com>
2020-08-19 10:36:28 +0800

    修改部分代码

--- platform-uc-auth/src/main/java/com/platform/uc/adapter/handler/BizRequestCache.java
+++ platform-uc-auth/src/main/java/com/platform/uc/adapter/handler/BizRequestCache.java
@@ -2,7 +2,9 @@ package com.platform.uc.adapter.handler;
 
 import com.platform.uc.adapter.contants.AuthorizationContacts;
 import com.ztkj.framework.response.utils.CookieUtils;
+import com.ztkj.framework.response.utils.SnowflakeIdUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.BooleanUtils;
 import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.web.PortResolver;
@@ -32,6 +34,8 @@ public class BizRequestCache implements RequestCache {
 
     private final PortResolver portResolver = new PortResolverImpl();
 
+    private final SnowflakeIdUtils snowflakeIdUtils = new SnowflakeIdUtils(0, 0);
+
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
@@ -40,19 +44,43 @@ public class BizRequestCache implements RequestCache {
      */
     @Override
     public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
-        if (requestMatcher.matches(request)) {
-            DefaultSavedRequest savedRequest = new DefaultSavedRequest(request, portResolver);
-            String ticket = String.valueOf(System.currentTimeMillis());
-
-            // 把卷放入cookie中
-            CookieUtils.set(response, AuthorizationContacts.TICKET, ticket, AuthorizationContacts.TICKET_EXPIRE.intValue());
-
-            // 保存到redis中
-            BoundValueOperations<String, Object> operations = redisTemplate.boundValueOps(ticket);
-            operations.set(savedRequest);
-        }else {
+        if (!requestMatcher.matches(request)) {
             log.debug("Request not saved as configured RequestMatcher did not match");
+            return;
         }
+        DefaultSavedRequest savedRequest = new DefaultSavedRequest(request, portResolver);
+        if (savedRequest.getParameterMap().containsKey(AuthorizationContacts.CLIENT_ID)){
+            // 参数
+            String[] values = savedRequest.getParameterValues(AuthorizationContacts.CLIENT_ID);
+            // 把卷放入cookie中
+            CookieUtils.set(response, AuthorizationContacts.CID, values[0], AuthorizationContacts.CID_EXPIRE.intValue());
+        }
+
+        // 判断卷是否存在
+        if (isExist(savedRequest)){
+            return;
+        }
+        // 生成缓存卷
+        String ticket = String.valueOf(snowflakeIdUtils.nextId());
+        // 把卷放入cookie中
+        CookieUtils.set(response, AuthorizationContacts.TICKET, ticket, AuthorizationContacts.TICKET_EXPIRE.intValue());
+        BoundValueOperations<String, Object> operations = redisTemplate.boundValueOps(AuthorizationContacts.REDIS_TICKET_KEY + ticket);
+        operations.set(savedRequest);
+    }
+
+    /**
+     * 判断ticket是否存在
+     */
+    private boolean isExist(DefaultSavedRequest savedRequest){
+        if (!savedRequest.getParameterMap().containsKey(AuthorizationContacts.TICKET)){
+            return false;
+        }
+        // 参数
+        String[] values = savedRequest.getParameterValues(AuthorizationContacts.TICKET);
+        String ticket = values[0];
+        // 判断ticket是否存在
+        Boolean isTrue = redisTemplate.hasKey(AuthorizationContacts.REDIS_TICKET_KEY + ticket);
+        return BooleanUtils.isFalse(isTrue);
     }
 
     /**
@@ -64,7 +92,7 @@ public class BizRequestCache implements RequestCache {
         if (StringUtils.isEmpty(ticket)){
             return null;
         }
-        BoundValueOperations<String, Object> operations = redisTemplate.boundValueOps(ticket);
+        BoundValueOperations<String, Object> operations = redisTemplate.boundValueOps(AuthorizationContacts.REDIS_TICKET_KEY + ticket);
         return (SavedRequest) operations.get();
     }
 
@@ -109,7 +137,7 @@ public class BizRequestCache implements RequestCache {
             return;
         }
         CookieUtils.set(response, AuthorizationContacts.TICKET, null, 0);
-        redisTemplate.delete(ticket);
+        redisTemplate.delete(AuthorizationContacts.REDIS_TICKET_KEY + ticket);
     }
 
 
